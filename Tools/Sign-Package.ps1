97b362767fb77dd2c9829f1b35239aea13132c9f
Nirmal Guru <Nirmal4G@gmail.com>
2018-05-31 22:14:01 +0530

    Spring Cleanup - Repo Structure (#78)
    * Restructure
    
    match to my repo so it's easy for PR compare
    
    * Fix Path to Extras props/targets
    
    Thereby fix Build
    
    * Build Updates
    
    Using Build structure from my repo for easy migration

---
+++ Tools/Sign-Package.ps1
@@ -0,0 +1,25 @@
+
+$currentDirectory = split-path $MyInvocation.MyCommand.Definition
+$repoDirectory = "$currentDirectory\..\"
+
+# See if we have the ClientSecret available
+if([string]::IsNullOrEmpty($env:SignClientSecret)){
+    Write-Host "Client Secret not found, not signing packages"
+    return;
+}
+
+& nuget install SignClient -Version 0.9.1 -SolutionDir "$repoDirectory" -Verbosity quiet -ExcludeVersion
+
+# Setup Variables we need to pass into the sign client tool
+
+$appSettings = "$currentDirectory\SignClient.json"
+$appPath = "$repoDirectory\packages\SignClient\tools\netcoreapp2.0\SignClient.dll"
+$nupgks = Get-ChildItem $Env:ArtifactDirectory\*.nupkg | Select-Object -ExpandProperty FullName
+
+foreach ($nupkg in $nupgks){
+    Write-Host "Submitting $nupkg for signing"
+    dotnet $appPath 'sign' -c $appSettings -i $nupkg -r $env:SignClientUser -s $env:SignClientSecret -n 'MSBuild.Sdk.Extras' -d 'MSBuild.Sdk.Extras' -u 'https://github.com/onovotny/MSBuildSdkExtras'
+    Write-Host "Finished signing $nupkg"
+}
+
+Write-Host "Sign-package complete"
