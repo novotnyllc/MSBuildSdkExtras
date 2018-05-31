4ece0b9c02c2eef93b829e411b38c0893d4ce939
Nirmal Guru <Nirmal4G@gmail.com>
2018-05-31 20:53:54 +0530

    Build Updates
    Using Build structure from my repo for easy migration

--- Tools/Sign-Package.ps1
+++ Tools/Sign-Package.ps1
@@ -1,27 +1,25 @@
+
 $currentDirectory = split-path $MyInvocation.MyCommand.Definition
+$repoDirectory = "$currentDirectory\..\"
 
 # See if we have the ClientSecret available
 if([string]::IsNullOrEmpty($env:SignClientSecret)){
-	Write-Host "Client Secret not found, not signing packages"
-	return;
+    Write-Host "Client Secret not found, not signing packages"
+    return;
 }
 
-& nuget install SignClient -Version 0.9.1 -SolutionDir "$currentDirectory\..\" -Verbosity quiet -ExcludeVersion
+& nuget install SignClient -Version 0.9.1 -SolutionDir "$repoDirectory" -Verbosity quiet -ExcludeVersion
 
 # Setup Variables we need to pass into the sign client tool
 
 $appSettings = "$currentDirectory\SignClient.json"
-
-$appPath = "$currentDirectory\..\packages\SignClient\tools\netcoreapp2.0\SignClient.dll"
-
-$nupgks = gci $Env:ArtifactDirectory\*.nupkg | Select -ExpandProperty FullName
+$appPath = "$repoDirectory\packages\SignClient\tools\netcoreapp2.0\SignClient.dll"
+$nupgks = Get-ChildItem $Env:ArtifactDirectory\*.nupkg | Select-Object -ExpandProperty FullName
 
 foreach ($nupkg in $nupgks){
-	Write-Host "Submitting $nupkg for signing"
-
-	dotnet $appPath 'sign' -c $appSettings -i $nupkg -r $env:SignClientUser -s $env:SignClientSecret -n 'MSBuild.Sdk.Extras' -d 'MSBuild.Sdk.Extras' -u 'https://github.com/onovotny/MSBuildSdkExtras' 
-
-	Write-Host "Finished signing $nupkg"
+    Write-Host "Submitting $nupkg for signing"
+    dotnet $appPath 'sign' -c $appSettings -i $nupkg -r $env:SignClientUser -s $env:SignClientSecret -n 'MSBuild.Sdk.Extras' -d 'MSBuild.Sdk.Extras' -u 'https://github.com/onovotny/MSBuildSdkExtras'
+    Write-Host "Finished signing $nupkg"
 }
 
 Write-Host "Sign-package complete"
